750a9ec6008cb489f3096e64b82731c042931d05
Carina.Nuyts <nuyts.carine@gmail.com>
2017-02-07 13:39:17 +0100

    na one-to-one

--- src/main/java/be/vdab/entities/Campus.java
+++ src/main/java/be/vdab/entities/Campus.java
@@ -9,11 +9,13 @@ import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
@@ -40,6 +42,9 @@ public class Campus implements Serializable {
 	// @JoinColumn(name = "campusid")
 	@OrderBy("voornaam, familienaam")
 	private Set<Docent> docenten;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "managerid")
+	private Manager manager;
 
 	public Campus(String naam, Adres adres) {
 		setNaam(naam);
@@ -89,16 +94,22 @@ public class Campus implements Serializable {
 
 	public void add(Docent docent) {
 		docenten.add(docent);
-		if (docent.getCampus() != this) { // als de andere kant nog niet bijgewerkt is
+		if (docent.getCampus() != this) { // als de andere kant nog niet
+											// bijgewerkt is
 			docent.setCampus(this); // werk je de andere kant bij
 		}
 	}
 
 	public void remove(Docent docent) {
 		docenten.remove(docent);
-		if (docent.getCampus() == this) { // als de andere kant nog niet bijgewerkt is
+		if (docent.getCampus() == this) { // als de andere kant nog niet
+											// bijgewerkt is
 			docent.setCampus(null); // werk je de andere kant bij
 		}
+	}
+
+	public Manager getManager() {
+		return manager;
 	}
 
 }
